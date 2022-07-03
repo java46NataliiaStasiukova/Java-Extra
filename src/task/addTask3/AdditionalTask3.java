e7f5dc9524be6952dc992d3e83e2cd24f1706fa4
Nataliia <nstasiukova@gmail.com>
2022-07-03 21:36:47 +0300

    AditionalTask-3.1

--- src/task/addTask3/AdditionalTask3.java
+++ src/task/addTask3/AdditionalTask3.java
@@ -2,7 +2,13 @@ package task.addTask3;
 
 public class AdditionalTask3 {
 	public static boolean isAnagram(String sourceWord, String targetWord) {
+		 String specialCharactersString = "!@#$%&*()'+,-./:;<=>?[]^_`{|}1234567890";
 		if(sourceWord.length() != targetWord.length()) return false;
+		for(int i = 0; i < sourceWord.length(); i++) {
+			if(specialCharactersString.indexOf(sourceWord.charAt(i)) >= 0) return false;
+			if(specialCharactersString.indexOf(targetWord.charAt(i)) >= 0) return false;
+		}
+		
 		StringBuilder str = new StringBuilder("");
 		int res = sourceWord.compareToIgnoreCase(str.append(targetWord).reverse().toString());
 		return res == 0 ? true : false;
--- src/task/addTask3/Tests/AditionalTask3Tests.java
+++ src/task/addTask3/Tests/AditionalTask3Tests.java
@@ -14,10 +14,12 @@ class AditionalTask3Tests {
 		String string2 = "oLLeh";
 		String string3 = "Helo";
 		String string4 = new String("olleh");
+		String string5 = "Helo%7";
 		assertTrue(AdditionalTask3.isAnagram(string1, string2));
 		assertFalse(AdditionalTask3.isAnagram(string1, string1));
 		assertFalse(AdditionalTask3.isAnagram(string1, string3));
 		assertTrue(AdditionalTask3.isAnagram(string1, string4));
+		assertFalse(AdditionalTask3.isAnagram(string1, string5));
 	}
 
 }
